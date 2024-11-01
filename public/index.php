bd319f3d925ecd189cb7d4296f0bcc0228371bcd
flapik19 <selivanovvlad3211@gmail.com>
2024-11-01 23:32:55 +0300

    make dataBase and make index.php

--- public/index.php
+++ public/index.php
@@ -1,2 +1,40 @@
 <?php 
 require 'vendor/autoload.php';
+
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    if (isset($_COOKIE['user_id']) && isset($_COOKIE['bg_color']) && isset($_COOKIE['font_color'])) {
+        $_SESSION['user_id'] = $_COOKIE['user_id'];
+        $_SESSION['bg_color'] = $_COOKIE['bg_color'];
+        $_SESSION['font_color'] = $_COOKIE['font_color'];
+    } else {
+        header("Location: pages/login.php");
+        exit();
+    }
+}
+
+$bgColor = isset($_SESSION['bg_color']) ? $_SESSION['bg_color'] : '#0040a1';
+$fontColor = isset($_SESSION['font_color']) ? $_SESSION['font_color'] : '$000000';
+?>
+
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+        <meta charset="UTF-8">
+        <meta name = "viewport" content = "width = device-width , initial-scale = 1.0">
+        <title>Лабораторная работа №1</title>
+        <style>
+            body { 
+                background-color: <? htmlspecialchars($bgColor) ?>;
+                color: <? htmlspecialchars($fontColor) ?>
+            }
+        </style>
+    </head>
+
+    <body>
+        <h1>Главная страница</h1>
+        <p>Сюда попадете после авторизация</p>
+        <a href = "pages/logout.php"></a>
+    </body>
+</html>
---
+++ public/src/DB/db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    bg_color VARCHAR(7) DEFAULT '#ffffff',
+    font_color VARCHAR(7) DEFAULT '#000000' 
+);
